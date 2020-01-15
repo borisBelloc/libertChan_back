2895f0fc70a5c2c59c773abdc2614607e97d8589
Nicolas <nicolas.gamel12450@gmail.com>
2020-01-15 15:50:16 +0100

    init

---
+++ src/main/java/kaboni/libertchan/LibertchanApplication.java
@@ -0,0 +1,13 @@
+package kaboni.libertchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibertchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibertchanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaboni/libertchan/LibertchanApplicationTests.java
@@ -0,0 +1,13 @@
+package kaboni.libertchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibertchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
