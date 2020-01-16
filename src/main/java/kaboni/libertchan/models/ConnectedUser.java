30e3dfb48faa1fca78db98431bc30a5de08c1517
elkhou <5069230-karimelkh@users.noreply.gitlab.com>
2020-01-16 11:43:36 +0100

    Revert "Revert "ajout du modèle ConnectedUser""
    This reverts commit 14f08b27c4ab4f2fa71e29bb8dde0eebee2a5054.

--- src/main/java/kaboni/libertchan/models/ConnectedUser.java
+++ src/main/java/kaboni/libertchan/models/ConnectedUser.java
@@ -1,5 +1,27 @@
 package kaboni.libertchan.models;
 
+import javax.persistence.*;
+
+@Entity
 public class ConnectedUser {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column
+	private String mail;
+	
+	@Column
+	private Integer karma;
+	
+	@Column
+	private String mainPseudo;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private Integer accessLevel;
 
 }
