c7c59359818434dfea0207164b4a7d65392b9104
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-19 10:13:37 +0100

    ajout des classes permettant l'upload de fichiers

--- src/main/java/kaboni/libertchan/LibertchanApplication.java
+++ src/main/java/kaboni/libertchan/LibertchanApplication.java
@@ -2,14 +2,18 @@ package kaboni.libertchan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import kaboni.libertchan.config.FileStorageProperties;
 
 @Configuration
 @SpringBootApplication
 @EnableWebMvc
 @EnableJpaRepositories
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class LibertchanApplication {
 
 	public static void main(String[] args) {
