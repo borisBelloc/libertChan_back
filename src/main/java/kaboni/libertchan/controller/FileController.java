4c6bb4ba8ce5b6ea90065327ac27c896b7a761c0
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-19 14:29:25 +0100

    ajout du cross origin pour le controller des images

--- src/main/java/kaboni/libertchan/controller/FileController.java
+++ src/main/java/kaboni/libertchan/controller/FileController.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class FileController {
