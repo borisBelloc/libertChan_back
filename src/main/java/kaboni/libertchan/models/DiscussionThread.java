d8219563806ca55dd4d57b4a337914b18cd8bc3c
Nicolas <nicolas.gamel12450@gmail.com>
2020-02-20 16:04:15 +0100

    modification des jsonIgnore

--- src/main/java/kaboni/libertchan/models/DiscussionThread.java
+++ src/main/java/kaboni/libertchan/models/DiscussionThread.java
@@ -13,6 +13,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 public class DiscussionThread {
@@ -43,7 +44,7 @@ public class DiscussionThread {
 	@OneToMany(mappedBy = "discussionThread", cascade = CascadeType.PERSIST)
 	private List<Message> messages;
 	
-	@JsonIgnore
+	@JsonIgnoreProperties("threads")
 	@ManyToOne
 	private Channel channel;
 
